4540cb36b312c1554ec489806d4f4a6213f37961
Natalia Nakollofski <115610226+NataliaN24@users.noreply.github.com>
2025-09-07 18:31:52 +0300

    Create 08.java

---
+++ Task 08/08.java
@@ -0,0 +1,12 @@
+public class SumChecker {
+    public static boolean hasEqualSum(int a,int b ,int c)
+    {
+        return a+b ==c; 
+    
+    }
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));  // false
+        System.out.println(hasEqualSum(1, 1, 2));  // true
+        System.out.println(hasEqualSum(1, -1, 0)); // true
+    }
+}
